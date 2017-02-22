39712d6b26c07ff427e15cc7adef66b2ef7a9af6
Anton Chernik <anton.chernik@amadeus.com>
2017-02-22 17:53:23 +0200

    Add jasny bootstrap

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
+
+import 'html5shiv';
 import 'bootstrap/less/bootstrap.less';
+//require("imports-loader?$=jquery!");
+//import 'jasny-bootstrap/less/jasny-bootstrap.less!imports-loader?bootstrap/less/bootstrap.less';
+
 console.log('Test message');
