57238129f42d9d5ed1471660fba025a093d1cc5f
Anton <anton.chernik@gmail.com>
2017-02-22 22:03:45 +0200

    Add jasny and bootstrap examples

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 'use strict';
 import jquery from 'jquery';
-import 'bootstrap/js/transition';
-import 'bootstrap/js/dropdown';
+import 'bootstrap';
+
 import 'jasny-bootstrap/js/transition';
 import 'jasny-bootstrap/js/offcanvas';
+import 'jasny-bootstrap/js/inputmask';
+import 'jasny-bootstrap/js/fileinput';
+import 'jasny-bootstrap/js/wizard';
 import 'html5shiv';
 import '../main.less';
 
